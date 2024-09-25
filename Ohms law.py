5f11bcf13c0428f88dad02463e103439adc1a18f
Qwakkhead <qwakkhead@example.com>
2024-09-25 18:28:20 +0800

    If zero division occur

--- Ohms law.py
+++ Ohms law.py
@@ -4,18 +4,26 @@ conversion_choice = input("Type 'I' to find current, 'V' to find voltage, and 'R
 if conversion_choice == 'I':
     Voltage = float(input("What is the value of your voltage: "))
     Resistance = float(input("What is the value of your resistance: "))
-    Current = Voltage / Resistance
-    print(f"Your current is: ", Current)
+    if Resistance == 0:
+        print("The Resistance cannot be zero")
+    else:
+        Current = Voltage / Resistance
+        print(f"Your current is: ", Current)
 
 elif conversion_choice == 'V':
     Resistance = float(input("What is the value of resistance: "))
     Current = float(input("What is the value of current: "))
     Voltage = Current * Resistance
     print(f"Your Voltage is ",Voltage)
+
 elif conversion_choice == 'R':
     Current = float(input("What is the value of the current: "))
     Voltage = float(input("What is the voltage: "))
-    Resistance = Voltage / Current
-    print("Your resistance is ", Resistance) 
+    if Current == 0:
+        print("The Current cannot be zero")
+    else:
+        Resistance = Voltage / Current
+        print("Your resistance is ", Resistance) 
 else:
     print("Invalid input ")
+
