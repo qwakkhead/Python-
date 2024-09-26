aa57516c4480ade39b676962997429d286f35785
Qwakkhead <qwakkhead@example.com>
2024-09-26 18:36:34 +0800

    Diamnond shape

---
+++ Diamond.py
@@ -0,0 +1,14 @@
+n = int(input("Numbers of rows: "))
+if n % 2 == 0:
+    print(" Odd integers only. ")
+else:
+    middle = n // 2
+    for i in range(middle + 1):
+        spaces = middle - i
+        stars = n - 2 * spaces
+        print(' ' * spaces + '*' * stars)
+    
+    for i in range(middle - 1, -1, -1):
+        spaces = middle - i
+        stars = n - 2 * spaces
+        print(' ' * spaces + '*' * stars)
